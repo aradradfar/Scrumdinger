34c4df6215332a0602d5a0d521f82a6ab3f54c59
Arad Radfar <aradradfar@gmail.com>
2022-03-02 14:10:01 +0330

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Arad Radfar on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
